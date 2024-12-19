1496d346a6073a0100c476837b3e5d6d6bbb3ed7
DepressedYoshi <jl2718281828459045@gmail.com>
2024-12-18 19:55:06 -0800

    complete data report

--- Customer.java
+++ Customer.java
@@ -1,18 +1,85 @@
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class Customer extends Thread {
+
+    /*
+     * TODO: 
+     * Shopping list 
+     * race 
+     * gender 
+     * karen 
+     * black 
+     * revenue 
+     */
     private static int nextId = 0;
     private final int id;
     private final long shopTime;
-    private final long enterTime;
+    private final String enterTime;
     private final long checkoutTime;
     private long enterQueTime;
     private long startCTime;
     private long leaveTime;
-
+    private String name;
+    private String race;
+    private final String[] names = {
+        "Karen", "Marha", "Lucas", "Carmen", "Josh", 
+        "Aisha", "Wei", "Hiroshi", "Fatima", "Carlos", 
+        "Amara", "Raj", "Liam", "Chen", "Sofia", 
+        "Miguel", "Alejandra", "Kwame", "Dae", "Zara", 
+        "Emily", "James", "Michael", "Jessica", "Robert"
+    };
+    private String[] races = {"White", "Asian", "Mexican", "Black", "Other"} ;
+    private ArrayList<Items> shoppingList;
+    
     public Customer() {
+        this.shoppingList = genShoppingList();
         this.id = nextId++;
-        this.shopTime = (int) (2000 + Math.random() * 15_000);
-        this.enterTime = System.currentTimeMillis();
-        this.checkoutTime = (int) (2000 + Math.random() * 3000);
+        this.shopTime = getShopTime(shoppingList);
+        this.enterTime = TimeUtil.getCurrentTime();
+        this.checkoutTime = getCheckoutTime(shoppingList);
+        this.name = genName();
+        this.race = genRace();
+    }
+
+    private String genName(){
+        return names[(int)(Math.random()*20)];
+    }
+    private String genRace(){
+        return races[(int)(Math.random()*5)];
+    }
+
+    private  long getShopTime(ArrayList<Items> list){
+        long sum =0;
+        for (Items items : list) {
+            sum +=items.getTime();
+        }
+        return sum;
+    }
+
+    private  long getCheckoutTime(ArrayList<Items> list){
+        int totalItem =0;
+        for (Items items : list) {
+            totalItem +=items.getQ();
+        }
+        return (long) (totalItem * (200+Math.random()*3000));
+    }
+
+    private ArrayList<Items> genShoppingList(){
+        ArrayList<Items> answer = new ArrayList<>();
+        int length = (int) (1 + Math.random() * 10);
+        for (int i = 0; i < length; i++) {
+            answer.add(new Items());
+        }
+        return answer;
+    }
+
+    public double getSubtotal(){;
+        double sum = 0;
+        for (Items items : shoppingList) {
+            sum += items.getPrice() * items.getQ();
+        }
+        return sum;
     }
 
     public long getCheckoutTime() {
@@ -22,14 +89,42 @@ public class Customer extends Thread {
     public long getWaitTime(){
         return startCTime - enterQueTime;
     }
-
+    public String getEnterTime(){
+        return this.enterTime;
+    }
     public void setLeaveTime(long leaveTime) {
         this.leaveTime = leaveTime;
     }
+    public long getShopTime() {
+        return shopTime;
+    }
+    public void setStartCTime(long startCTime) {
+        this.startCTime = startCTime;
+    }
 
+    public String printSummary(){        
+        /*
+         * Customer-0 Information:
+         * Name: 
+         *      - if karen - chekc for chekc out time 
+         * Race: 
+         *      - if black - check for profit 
+         * Shoptime:
+         * Waittime:
+         * Enter Time:
+         * Leave Tiem: 
+         * 
+        */
+        return this + " information: \n" + "Name: " + name + 
+                        "\nRaces: " + race +
+                        "\nShopTime: " +  TimeUtil.beautifyMili(shopTime) + 
+                        "\nWaitime: " + TimeUtil.beautifyMili(this.getWaitTime()) + 
+                        "\nItems Purchased: " + shoppingList.toString() + "\n";
+    }
     @Override
     public String toString() {
-        return "Customer" + id + ": (enterTime=" + enterTime % 100_000 + ", shopTime=" + shopTime + ")";
+        //todo
+        return "Customer-" + id;
     }
 
     @Override
@@ -41,6 +136,6 @@ public class Customer extends Thread {
         }
         Main.addToQ(this);
         this.enterQueTime = System.currentTimeMillis();
-        System.out.println(this + " enters que at " + enterQueTime);
+        System.out.println( "[" + TimeUtil.getCurrentTime() + "] " + this + " enters the que");
     }
 }
--- Main.java
+++ Main.java
@@ -6,55 +6,105 @@ public class Main {
     private static final Object queueLock = new Object();
     private static long simTime = 30_000;
     private static volatile boolean isCustomerFactoryDone = false; // Tracks when CustomerFactory finishes
-    static CustomerFactory customerFactory = new CustomerFactory(simTime, queue, queueLock);
+    static CustomerFactory customerFactory = new CustomerFactory(simTime, queue, queueLock);    
+    private static int lanes = 3;
     
-        /*
-         * - how long is each customer in line for 
-        - how does changing the number of cashiers effect wait times 
-        - total number of customers
-        - how long did customer shop for 
-         */
-    
-    
-    
-        public static void main(String[] args) throws InterruptedException {
-            queue = new LinkedList<>();
-            int lanes = 3;
-    
-            Cashier[] cashiers = new Cashier[lanes];
-    
-            for (int i = 0; i < lanes; i++) {
-                cashiers[i] = new Cashier(queue, queueLock, i + 1);
-                cashiers[i].start();
-            }
-            customerFactory.start();
+            public static void main(String[] args) throws InterruptedException {
+                queue = new LinkedList<>();
+                Cashier[] cashiers = new Cashier[lanes];
 
-            System.out.println("finished");
-
-    }
-
-    public static synchronized boolean isCustomerFactoryDone() {
-        return isCustomerFactoryDone;
-    }
+                System.out.println("[" + TimeUtil.getCurrentTime() + "] Yoshi's El Supremo Shein Factory Grocery is Open For Business !!!!!!!");
+        
+                for (int i = 0; i < lanes; i++) {
+                    cashiers[i] = new Cashier(queue, queueLock, i + 1);
+                    cashiers[i].start();
+                }
+                customerFactory.start();
     
-    public static synchronized void setCustomerFactoryDone(boolean isCustomerFactoryDone) {
-        Main.isCustomerFactoryDone = isCustomerFactoryDone;
-    }
-
-    public static synchronized boolean noCustomerShopping(Cashier cashier){
-        //check if there are still customer thread running on shopping time and yet enbtered the que 
-        return customerFactory.getCustomerCount() <= cashier.getCustomerCount();
-    }
-
-    public static synchronized void addToQ(Customer customer) {
-        synchronized (queueLock) {
-            queue.add(customer);
-            queueLock.notify(); // Update the Linked list for the Cashier class
         }
-    }
+    
+        public static synchronized boolean isCustomerFactoryDone() {
+            return isCustomerFactoryDone;
+        }
+        
+        public static synchronized void setCustomerFactoryDone(boolean isCustomerFactoryDone) {
+            Main.isCustomerFactoryDone = isCustomerFactoryDone;
+        }
+    
+        public static synchronized boolean noCustomerShopping(Cashier cashier){
+            //check if there are still customer thread running on shopping time and yet enbtered the que 
+            return customerFactory.getCustomerCount() <= cashier.getCustomerCount();
+        }
+    
+        public static synchronized void addToQ(Customer customer) {
+            synchronized (queueLock) {
+                queue.add(customer);
+                queueLock.notify(); // Update the Linked list for the Cashier class
+            }
+        }
 
-    public static void printStat(ArrayList<Customer> finalist){
+        private static void printEachCustomerStat(ArrayList<Customer> finalist){
+            for (Customer c : finalist) {
+                System.out.println(c.printSummary());
+            }
+        }
 
+        private static long calculateShopTime(ArrayList<Customer> list){
+            long sum = 0;
+            for (Customer customer : list) {
+                sum += customer.getShopTime();
+            }
+
+            return sum / customerFactory.getCustomerCount();
+        }
+
+        private static long calculateCOutTime(ArrayList<Customer> list){
+            long sum = 0;
+            for (Customer customer : list) {
+                sum += customer.getCheckoutTime();
+            }
+
+            return sum / customerFactory.getCustomerCount();
+        }
+        private static long calculateWaitTime(ArrayList<Customer> list){
+            long sum = 0;
+            for (Customer customer : list) {
+                sum += customer.getWaitTime();
+            }
+
+            return sum / customerFactory.getCustomerCount();
+        }
+        private static double getReveue(ArrayList<Customer> list){
+            double sum = 0;
+            for (Customer customer : list) {
+                sum += customer.getSubtotal();
+            }
+            return sum;
+        }
+        private static double getProfit(ArrayList<Customer> list){
+            double sum = 0;
+            double profitMargin = Items.getProfit();
+            for (Customer customer : list) {
+                sum += customer.getSubtotal();
+            }
+            return sum * profitMargin;
+        }
+
+    
+
+        public static void printStat(ArrayList<Customer> finalist){
+            System.out.println("--------------------------------------------STAT--------------------------------------------");
+            System.out.println("=========Overview=========");
+            System.out.println("There were " + lanes +" lnes of Cashiers");
+            System.out.println("Total Number of Customer Entered: " + customerFactory.getCustomerCount());
+            System.out.println("Time Store Stayed Opened: " + TimeUtil.beautifyMili(simTime));
+            System.out.println("Average Shopping Time: " + TimeUtil.beautifyMili(calculateShopTime(finalist)));
+            System.out.println("Average Waiting Time: " + TimeUtil.beautifyMili(calculateWaitTime(finalist)));
+            System.out.println("Average Checkout Time: " + TimeUtil.beautifyMili(calculateCOutTime(finalist)));
+            System.out.println("Total Reveunue: $" + getReveue(finalist));
+            System.out.println("Total Profit: $" + getProfit(finalist));
+            System.out.println("=========Customer Activity Log========="); 
+            printEachCustomerStat(finalist);
     }
 
    
